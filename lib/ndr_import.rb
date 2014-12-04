4172aa51c389c0901ffd98a1e4dfe9adea55d9d7
tim.gentry <tim.gentry@153ec483-9fa4-4787-9925-b21d4235b228>
2014-12-04 09:22:24 +0000

    # Added MappingError class
    git-svn-id: https://deepthought/svn/non-era/gems/ndr_import@2593 153ec483-9fa4-4787-9925-b21d4235b228

--- lib/ndr_import.rb
+++ lib/ndr_import.rb
@@ -1,1 +1,2 @@
 require 'ndr_import/version'
+require 'ndr_import/mapping_error'
---
+++ lib/ndr_import/mapping_error.rb
@@ -0,0 +1,7 @@
+# Raised if there is a problem with an import mapping.
+module UnifiedSources
+  module Import
+    class MappingError < StandardError
+    end
+  end
+end
