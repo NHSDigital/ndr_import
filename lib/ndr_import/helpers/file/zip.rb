3453ad3c16769786ce51b6eec08c8e6d5ed4d67f
tim.gentry <tim.gentry@153ec483-9fa4-4787-9925-b21d4235b228>
2014-12-22 13:52:31 +0000

    # Added unzip_file as is
    git-svn-id: https://deepthought/svn/non-era/gems/ndr_import@2674 153ec483-9fa4-4787-9925-b21d4235b228

---
+++ lib/ndr_import/helpers/file/zip.rb
@@ -0,0 +1,41 @@
+require 'ndr_support/safe_file'
+
+module NdrImport
+  module Helpers
+    module File
+      # This mixin adds Zip functionality to unified importers.
+      module Zip
+        # Unzip the file, creating the destination directory if necessary.
+        # A pattern can be provided to only extract required files.
+        def unzip_file(source, destination, pattern = //)
+          # SECURE TVB Mon Aug 13 14:41:05 BST 2012 : SafePath will raise exception if insecure path is constructed
+          # SafeFile.safepath_to_string will make sure that the arguments are from type SafePath
+    
+          # SECURE: BNS 2010-09-21 (for external access)
+          raise 'Not allowed in external environment' if Rails.env.external?
+    
+          require 'zip/zipfilesystem'
+          # TODO: Abort if destination directory already exists...
+          FileUtils.mkdir_p(SafeFile.safepath_to_string(destination))
+    
+          Zip::ZipFile.open(SafeFile.safepath_to_string(source)) do |zipfile|
+            zipfile.entries.each do |entry|
+              # SECURE: TPG 2010-11-1: The path is stripped from the zipfile entry when extracted
+              basename = File.basename(entry.name)
+              zipfile.extract(entry, destination.join(basename)) if entry.file? && basename.match(pattern)
+            end
+          end
+    
+        rescue Zip::ZipDestinationFileExistsError => ex
+          # I'm going to ignore this and just overwrite the files.
+        rescue SecurityError => ex
+          raise ex
+        rescue ArgumentError => ex
+          raise ex
+        rescue => ex
+          puts ex
+        end
+      end
+    end
+  end
+end
