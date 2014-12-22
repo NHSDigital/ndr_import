c15fbceb7a179e3c901a10e2bb7dc7cf939e4e79
tim.gentry <tim.gentry@153ec483-9fa4-4787-9925-b21d4235b228>
2014-12-22 13:52:37 +0000

    # Changed test style for consistency
    git-svn-id: https://deepthought/svn/non-era/gems/ndr_import@2675 153ec483-9fa4-4787-9925-b21d4235b228

--- test/helpers/file/pdf_test.rb
+++ test/helpers/file/pdf_test.rb
@@ -6,10 +6,6 @@ class PdfTest < ActiveSupport::TestCase
   # This is a test importer class to test the PDF file helper mixin
   class TestImporter
     include NdrImport::Helpers::File::Pdf
-
-    def read_pdf_file_content(path)
-      read_pdf_file(path)
-    end
   end
 
   def setup
@@ -18,13 +14,13 @@ class PdfTest < ActiveSupport::TestCase
   end
 
   test 'read_pdf_file helper should read pdf file' do
-    file_content = @importer.read_pdf_file_content(@permanent_test_files.join('hello_world.pdf'))
+    file_content = @importer.send(:read_pdf_file, @permanent_test_files.join('hello_world.pdf'))
     assert_equal file_content, ['Hello World']
   end
 
   test 'read_pdf_file helper should raise exception on invalid pdf file' do
-    assert_raise RuntimeError do
-      @importer.read_pdf_file_content(@permanent_test_files.join('not_a_pdf.pdf'))
+    assert_raises RuntimeError do
+      @importer.send(:read_pdf_file, @permanent_test_files.join('not_a_pdf.pdf'))
     end
   end
 end
