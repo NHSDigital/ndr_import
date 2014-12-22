f755c6960182f7dd460c18866cccfdf09178e860
tim.gentry <tim.gentry@153ec483-9fa4-4787-9925-b21d4235b228>
2014-12-22 13:52:47 +0000

    # Added delimited file helper tests and test files
    git-svn-id: https://deepthought/svn/non-era/gems/ndr_import@2684 153ec483-9fa4-4787-9925-b21d4235b228

---
+++ test/helpers/file/delimited_test.rb
@@ -0,0 +1,36 @@
+require 'test_helper'
+require 'ndr_import/helpers/file/delimited'
+
+# Delimited file helper tests
+class DelimitedTest < ActiveSupport::TestCase
+  # This is a test importer class to test the Delimited file helper mixin
+  class TestImporter
+    include NdrImport::Helpers::File::Delimited
+  end
+
+  def setup
+    @permanent_test_files = SafePath.new('permanent_test_files')
+    @importer  = TestImporter.new
+  end
+
+  test 'should read csv correctly' do
+    rows = @importer.send(:read_delimited_file, @permanent_test_files.join('normal.csv'), nil)
+    assert_equal ('A'..'Z').to_a, rows[0]
+    assert_equal ['1'] * 26, rows[1]
+    assert_equal ['2'] * 26, rows[2]
+  end
+
+  test 'should read csv with a BOM' do
+    rows = @importer.send(:read_delimited_file, @permanent_test_files.join('bomd.csv'), nil)
+    assert_equal ('A'..'Z').to_a, rows[0]
+    assert_equal ['1'] * 26, rows[1]
+    assert_equal ['2'] * 26, rows[2]
+  end
+
+  test 'should read windows-1252 csv' do
+    assert_nothing_raised do
+      rows = @importer.send(:read_delimited_file, @permanent_test_files.join('windows.csv'), nil)
+      assert_equal 1, rows.length
+    end
+  end
+end
