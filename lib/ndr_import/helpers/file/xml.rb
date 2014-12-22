d49030f99b98d67a91a0b6587dc3342519f155fb
tim.gentry <tim.gentry@153ec483-9fa4-4787-9925-b21d4235b228>
2014-12-22 17:41:51 +0000

    # Housekeeping
    git-svn-id: https://deepthought/svn/non-era/gems/ndr_import@2692 153ec483-9fa4-4787-9925-b21d4235b228

--- lib/ndr_import/helpers/file/xml.rb
+++ lib/ndr_import/helpers/file/xml.rb
@@ -14,12 +14,12 @@ module NdrImport
           bad_re = Regexp.new("[#{bad_ctrl_codes.collect(&:chr).join}]")
           if file_data[0..1] == "\377\376" || file_data[0..1] == "\376\377"
             require 'iconv'
-            ic = Iconv.new("UTF-8", "UTF-16")
+            ic = Iconv.new('UTF-8', 'UTF-16')
             if ic.iconv(file_data) =~ bad_re
               msg = "UnifiedSources::Import::FileImporter#read_xml_file: Error: Cannot handle control characters in UTF-16 file #{path.inspect}"
               Rails.logger.warn(msg)
               puts msg
-              raise "Invalid control characters in UTF-16 file."
+              fail 'Invalid control characters in UTF-16 file.'
             end
           elsif file_data =~ bad_re
             # Replace invalid XML control characters with escaped equivalents.
@@ -27,14 +27,14 @@ module NdrImport
             # chr(0) is always invalid, but at least it doesn't break Nokogiri.
             # http://www.w3.org/International/questions/qa-controls
             bad_found = []
-            bad_ctrl_codes.each{|ctrl|
+            bad_ctrl_codes.each do |ctrl|
               bad_chr = ctrl.chr
               if file_data.index(bad_chr)
                 bad_found << ctrl
                 escaped = "&#x%04x;" % ctrl # e.g. "&#x0019;" for chr(25)
                 file_data = file_data.gsub(bad_chr, escaped)
               end
-            }
+            end
             msg = "UnifiedSources::Import::FileImporter#read_xml_file: Warning: Replaced invalid control characters with codes #{bad_found.inspect} in file #{path.inspect}"
             Rails.logger.warn(msg)
             puts msg
--- test/helpers/file/xml_test.rb
+++ test/helpers/file/xml_test.rb
@@ -10,44 +10,52 @@ class XmlTest < ActiveSupport::TestCase
   end
 
   def setup
-    @home = SafePath.new("test_space_rw")
+    @home = SafePath.new('test_space_rw')
     @permanent_test_files = SafePath.new('permanent_test_files')
     @importer  = TestImporter.new
   end
 
   test '.import_xml_file should reject non safe path arguments' do
     assert_raises ArgumentError do
-      @importer.send(:read_xml_file, @home.join("simple.xml").to_s)
+      @importer.send(:read_xml_file, @home.join('simple.xml').to_s)
     end
   end
 
   test '.import_xml_file should accept safepath' do
     builder = Nokogiri::XML::Builder.new do |xml|
-      xml.root {
-        xml.note(:id => 1) {
-          xml.time "Thu Dec 13 13:12:00 UTC 2012"
-          xml.title "Note 1"
-          xml.body "Note 1 body blabla bla"
-        }
-        xml.note(:id => 2) {
-          xml.time "Thu Dec 14 12:11:00 UTC 2012"
-          xml.title "note 2"
-          xml.body "note 2 body blablabala"
-        }
-      }
+      xml.root do
+        xml.note(:id => 1) do
+          xml.time 'Thu Dec 13 13:12:00 UTC 2012'
+          xml.title 'Note 1'
+          xml.body 'Note 1 body blabla bla'
+        end
+        xml.note(:id => 2) do
+          xml.time 'Thu Dec 14 12:11:00 UTC 2012'
+          xml.title 'note 2'
+          xml.body 'note 2 body blablabala'
+        end
+      end
     end
-    SafeFile.open(@home.join("simple.xml"), "w") {|f| f.write builder.to_xml}
+    SafeFile.open(@home.join('simple.xml'), 'w') { |f| f.write builder.to_xml }
 
+    doc = @importer.send(:read_xml_file, @home.join('simple.xml'))
 
-    doc = @importer.send(:read_xml_file, @home.join("simple.xml"))
+    assert_equal 1, doc.children.reject { |c| c.text =~ /\A\n *\Z/ }.length
+    assert_equal 'root', doc.children.reject { |c| c.text =~ /\A\n *\Z/ }[0].name
+    assert_equal 2, doc.
+      children.reject { |c| c.text =~ /\A\n *\Z/ }[0].
+      children.reject { |c| c.text =~ /\A\n *\Z/ }.length
+    assert_equal 'note', doc.children.reject { |c| c.text =~ /\A\n *\Z/ }[0].
+      children.reject { |c| c.text =~ /\A\n *\Z/ }[0].name
+    assert_equal 3, doc.
+      children.reject { |c| c.text =~ /\A\n *\Z/ }[0].
+      children.reject { |c| c.text =~ /\A\n *\Z/ }[0].
+      children.reject { |c| c.text =~ /\A\n *\Z/ }.length
+    assert_equal 'Thu Dec 13 13:12:00 UTC 2012', doc.
+      children.reject { |c| c.text =~ /\A\n *\Z/ }[0].
+      children.reject { |c| c.text =~ /\A\n *\Z/ }[0].
+      children.reject { |c| c.text =~ /\A\n *\Z/ }[0].text
 
-    assert_equal 1, doc.children.reject{ |c| c.text =~ /\A\n *\Z/}.length
-    assert_equal "root", doc.children.reject{ |c| c.text =~ /\A\n *\Z/}[0].name
-    assert_equal 2, doc.children.reject{ |c| c.text =~ /\A\n *\Z/}[0].children.reject{ |c| c.text =~ /\A\n *\Z/}.length
-    assert_equal "note", doc.children.reject{ |c| c.text =~ /\A\n *\Z/}[0].children.reject{ |c| c.text =~ /\A\n *\Z/}[0].name
-    assert_equal 3, doc.children.reject{ |c| c.text =~ /\A\n *\Z/}[0].children.reject{ |c| c.text =~ /\A\n *\Z/}[0].children.reject{ |c| c.text =~ /\A\n *\Z/}.length
-    assert_equal "Thu Dec 13 13:12:00 UTC 2012", doc.children.reject{ |c| c.text =~ /\A\n *\Z/}[0].children.reject{ |c| c.text =~ /\A\n *\Z/}[0].children.reject{ |c| c.text =~ /\A\n *\Z/}[0].text
-
-    SafeFile.delete @home.join("simple.xml")
+    SafeFile.delete @home.join('simple.xml')
   end
 end
