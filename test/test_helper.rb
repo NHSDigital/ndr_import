c244390ab15a9745d4e8c4cbfbaff67d7ef80cb4
Josh Pencheon <josh.pencheon@googlemail.com>
2015-11-19 22:23:21 +0000

    # Ensure tests run on Rails 4+. Closes #2

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -7,6 +7,17 @@ require 'ndr_support/safe_path'
 require 'ndr_import'
 require 'yaml'
 
+begin
+  # Shim for Test::Unit vs. Minitest:
+  require 'active_support/testing/autorun'
+rescue LoadError
+  # Rails 4+ only
+end
+
+# The default changes to UTC in Rails 4.
+# TODO: ndr_support should cope...
+ActiveRecord::Base.default_timezone = :local
+
 SafePath.configure! File.dirname(__FILE__) + '/resources/filesystem_paths.yml'
 NdrImport::StandardMappings.mappings = YAML.load_file(
   File.expand_path(File.dirname(__FILE__) + '/resources/standard_mappings.yml')
