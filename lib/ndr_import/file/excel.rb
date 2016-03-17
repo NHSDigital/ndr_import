e108b1635e41e89e4cba816d47514dcd9d0d6433
Josh Pencheon <josh.pencheon@googlemail.com>
2016-03-17 13:54:07 +0000

    # Avoid re-reading XLS sheets, regression from 334d53bb9.

--- lib/ndr_import/file/excel.rb
+++ lib/ndr_import/file/excel.rb
@@ -64,12 +64,12 @@ module NdrImport
       def xls_rows(workbook, sheet_name)
         return enum_for(:xls_rows, workbook, sheet_name) unless block_given?
 
-        activate_sheet!(workbook, sheet_name)
-        return unless workbook.first_row
-        (workbook.first_row..workbook.last_row).each do |row|
-          activate_sheet!(workbook, sheet_name)
-          columns = (workbook.first_column..workbook.last_column)
-          yield columns.map { |col| cast_excel_value(workbook.cell(row, col)) }
+        return unless workbook.first_row(sheet_name)
+        rows    = workbook.first_row(sheet_name)..workbook.last_row(sheet_name)
+        columns = workbook.first_column(sheet_name)..workbook.last_column(sheet_name)
+
+        rows.each do |row|
+          yield columns.map { |col| cast_excel_value(workbook.cell(row, col, sheet_name)) }
         end
       end
 
@@ -78,9 +78,7 @@ module NdrImport
       def xlsx_rows(workbook, sheet_name)
         return enum_for(:xlsx_rows, workbook, sheet_name) unless block_given?
 
-        activate_sheet!(workbook, sheet_name)
-
-        workbook.each_row_streaming(:pad_cells => true) do |row|
+        workbook.each_row_streaming(sheet: sheet_name, pad_cells: true) do |row|
           yield row.map { |cell| cast_excel_value(cell.value) }
         end
       end
@@ -125,14 +123,6 @@ module NdrImport
         # SafeFile will make sure that the arguments are actually SafePath
         FileUtils.mkdir_p(SafeFile.safepath_to_string(SafeFile.dirname(dest)))
         FileUtils.cp(SafeFile.safepath_to_string(source), SafeFile.safepath_to_string(dest))
-      end
-
-      # Sets `sheet_name' to be the default (i.e. active) sheet in `workbook'.
-      # This setting applies to the Roo object, so needs to re-applied if (for example)
-      # streaming rows from multiple sheets simultaneously - something that was problematic
-      # with Workbook#each_with_pagename.
-      def activate_sheet!(workbook, sheet_name)
-        workbook.sheet(sheet_name)
       end
     end
 

NHSDigital/ndr_import

commit 334d53bb9e8698bba3d92b4dbd71b36faacae629

# Fix issues when streaming from multiple XLS/XLSX sheets simultaneously.

lib/ndr_import/file/excel.rb
@@ -18,8 +18,8 @@ module NdrImport
         return enum_for(:tables) unless block_given?
 
         workbook = load_workbook(@filename)
-        workbook.each_with_pagename do |tablename, sheet|
-          yield tablename, excel_rows(workbook, sheet)
+        workbook.sheets.each do |sheet_name|
+          yield sheet_name, excel_rows(workbook, sheet_name)
         end
       end
 
@@ -49,37 +49,38 @@ module NdrImport
       private
 
       # Iterate through the sheet line by line, yielding each one in turn.
-      def excel_rows(workbook, sheet, &block)
-        return enum_for(:excel_rows, workbook, sheet) unless block
+      def excel_rows(workbook, sheet_name, &block)
+        return enum_for(:excel_rows, workbook, sheet_name) unless block
 
         if workbook.is_a?(Roo::Excelx)
           # FIXME: xlsx_rows(sheet, &block) should produce the same output as xls_rows
-          xls_rows(sheet, &block)
+          xls_rows(workbook, sheet_name, &block)
         else
-          xls_rows(sheet, &block)
+          xls_rows(workbook, sheet_name, &block)
         end
       end
 
       # Iterate through an xls sheet line by line, yielding each one in turn.
-      def xls_rows(sheet)
-        return enum_for(:xls_rows, sheet) unless block_given?
-        return unless sheet.first_row # NULL Sheet (TODO: perhaps XLSX only, use xlsx_rows...)
+      def xls_rows(workbook, sheet_name)
+        return enum_for(:xls_rows, workbook, sheet_name) unless block_given?
 
-        sheet.first_row.upto(sheet.last_row) do |row|
-          line = []
-          sheet.first_column.upto(sheet.last_column) do |col|
-            line << cast_excel_value(sheet.cell(row, col))
-          end
-          yield line
+        activate_sheet!(workbook, sheet_name)
+        return unless workbook.first_row
+        (workbook.first_row..workbook.last_row).each do |row|
+          activate_sheet!(workbook, sheet_name)
+          columns = (workbook.first_column..workbook.last_column)
+          yield columns.map { |col| cast_excel_value(workbook.cell(row, col)) }
         end
       end
 
       # Iterate through an xlsx sheet line by line, yielding each one in turn.
       # This method uses streaming https://github.com/roo-rb/roo#excel-xlsx-and-xlsm-support
-      def xlsx_rows(sheet)
-        return enum_for(:xlsx_rows, sheet) unless block_given?
+      def xlsx_rows(workbook, sheet_name)
+        return enum_for(:xlsx_rows, workbook, sheet_name) unless block_given?
 
-        sheet.each_row_streaming(:pad_cells => true) do |row|
+        activate_sheet!(workbook, sheet_name)
+
+        workbook.each_row_streaming(:pad_cells => true) do |row|
           yield row.map { |cell| cast_excel_value(cell.value) }
         end
       end
@@ -124,6 +125,14 @@ module NdrImport
         # SafeFile will make sure that the arguments are actually SafePath
         FileUtils.mkdir_p(SafeFile.safepath_to_string(SafeFile.dirname(dest)))
         FileUtils.cp(SafeFile.safepath_to_string(source), SafeFile.safepath_to_string(dest))
+      end
+
+      # Sets `sheet_name' to be the default (i.e. active) sheet in `workbook'.
+      # This setting applies to the Roo object, so needs to re-applied if (for example)
+      # streaming rows from multiple sheets simultaneously - something that was problematic
+      # with Workbook#each_with_pagename.
+      def activate_sheet!(workbook, sheet_name)
+        workbook.sheet(sheet_name)
       end
     end
 

test/file/excel_test.rb
@@ -33,9 +33,28 @@ module NdrImport
         file_path = @permanent_test_files.join('blank_tab_test.xlsx')
         handler   = NdrImport::File::Excel.new(file_path, nil)
 
-        handler.tables.each do |_tablename, sheet|
-          assert_instance_of Enumerator, sheet
-          assert sheet.to_a.is_a?(Array)
+        sheets = handler.tables.map { |_tablename, sheet| sheet }
+
+        assert_equal %w(column_a column_b column_c column_d), sheets[0].next
+        assert_equal %w(11111 ABC123 8888888888 2), sheets[0].next
+        assert_equal %w(column_a column_b column_c column_d), sheets[1].next
+        assert_equal %w(11111 ABC123 8888888888 3), sheets[1].next
+        assert_raises(StopIteration) { sheets[2].next }
+      end
+
+      %w(sheet_streaming.xlsx sheet_streaming.xls).each do |filename|
+        test "should be able to stream from multiple sheets at once from #{filename}" do
+          file_path = @permanent_test_files.join(filename)
+          handler   = NdrImport::File::Excel.new(file_path, nil)
+
+          sheets = handler.tables.map { |_tablename, sheet| sheet }
+
+          assert_equal %w(1A1 1B1), sheets[0].next
+          assert_raises(StopIteration) { sheets[2].next }
+          assert_equal %w(2A1 2B1), sheets[1].next
+          assert_equal %w(2A2 2B2), sheets[1].next
+          assert_equal %w(1A2 1B2), sheets[0].next
+          assert_raises(StopIteration) { sheets[2].next }
         end
       end
 

commit e108b1635e41e89e4cba816d47514dcd9d0d6433

# Avoid re-reading XLS sheets, regression from 334d53bb9.

lib/ndr_import/file/excel.rb
@@ -64,12 +64,12 @@ module NdrImport
       def xls_rows(workbook, sheet_name)
         return enum_for(:xls_rows, workbook, sheet_name) unless block_given?
 
-        activate_sheet!(workbook, sheet_name)
-        return unless workbook.first_row
-        (workbook.first_row..workbook.last_row).each do |row|
-          activate_sheet!(workbook, sheet_name)
-          columns = (workbook.first_column..workbook.last_column)
-          yield columns.map { |col| cast_excel_value(workbook.cell(row, col)) }
+        return unless workbook.first_row(sheet_name)
+        rows    = workbook.first_row(sheet_name)..workbook.last_row(sheet_name)
+        columns = workbook.first_column(sheet_name)..workbook.last_column(sheet_name)
+
+        rows.each do |row|
+          yield columns.map { |col| cast_excel_value(workbook.cell(row, col, sheet_name)) }
         end
       end
 
@@ -78,9 +78,7 @@ module NdrImport
       def xlsx_rows(workbook, sheet_name)
         return enum_for(:xlsx_rows, workbook, sheet_name) unless block_given?
 
-        activate_sheet!(workbook, sheet_name)
-
-        workbook.each_row_streaming(:pad_cells => true) do |row|
+        workbook.each_row_streaming(sheet: sheet_name, pad_cells: true) do |row|
           yield row.map { |cell| cast_excel_value(cell.value) }
         end
       end
@@ -125,14 +123,6 @@ module NdrImport
         # SafeFile will make sure that the arguments are actually SafePath
         FileUtils.mkdir_p(SafeFile.safepath_to_string(SafeFile.dirname(dest)))
         FileUtils.cp(SafeFile.safepath_to_string(source), SafeFile.safepath_to_string(dest))
-      end
-
-      # Sets `sheet_name' to be the default (i.e. active) sheet in `workbook'.
-      # This setting applies to the Roo object, so needs to re-applied if (for example)
-      # streaming rows from multiple sheets simultaneously - something that was problematic
-      # with Workbook#each_with_pagename.
-      def activate_sheet!(workbook, sheet_name)
-        workbook.sheet(sheet_name)
       end
     end
 