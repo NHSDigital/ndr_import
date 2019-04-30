5e695c9b0965354faf310f6e5a3a62ce7ad9c646
timgentry <tpgentry@me.com>
2019-04-30 13:09:23 +0100

    # Added decryption of password protected docx files

--- lib/ndr_import/file/docx.rb
+++ lib/ndr_import/file/docx.rb
@@ -1,5 +1,6 @@
 require 'docx'
 require 'ndr_support/safe_file'
+require_relative 'office_file_helper'
 require_relative 'registry'
 
 module NdrImport
@@ -9,18 +10,34 @@ module NdrImport
     # This class is a modern Word document file handler that returns a single table.
     # It only works on .docx documents
     class Docx < Base
+      include OfficeFileHelper
+
       private
 
       def rows(&block)
         return enum_for(:rows) unless block
 
-        doc = ::Docx::Document.open(SafeFile.safepath_to_string(@filename))
+        doc = if @options.key?(:file_password)
+                decrypted_docx_document(@filename, @options[:file_password])
+              else
+                ::Docx::Document.open(SafeFile.safepath_to_string(@filename))
+              end
 
         doc.paragraphs.each do |p|
           yield(p.to_s)
         end
       rescue StandardError => e
         raise("#{SafeFile.basename(@filename)} [#{e.class}: #{e.message}]")
+      end
+
+      # This method returns the Docx::Document of a password protected docx file
+      def decrypted_docx_document(encrypted_path, password)
+        Tempfile.create(['decrypted', '.docx']) do |file|
+          file.write(decrypted_file_string(encrypted_path, password))
+          file.close
+
+          return ::Docx::Document.open(file.path)
+        end
       end
     end
 
