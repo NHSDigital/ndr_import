9d24c7c4a3ebf347e9e2949a61ed6433bb42ba08
Josh Pencheon <josh.pencheon@googlemail.com>
2019-07-26 07:46:56 +0100

    # add p.o.c. xml_streaming helper
    Aims to improve XML performance, by avoiding the need for nokogiri to hold in memory and entire XML DOM - often 10x+ larger than the raw XML data

---
+++ lib/ndr_import/helpers/file/xml_streaming.rb
@@ -0,0 +1,67 @@
+require 'ndr_support/safe_file'
+require 'ndr_support/utf8_encoding'
+
+module NdrImport
+  module Helpers
+    module File
+      # This mixin adds XML streaming functionality to unified importers.
+      module XmlStreaming
+        private
+
+        def add_nodes(xml, nodes)
+          name, attributes = *nodes.shift
+          xml.send(name, attributes) { add_nodes(xml, nodes) if nodes.any? }
+        end
+
+        def stubs
+          # Stubs at each nesting, to apply xpath to:
+          @stubs ||= Hash.new do |hash, nodes|
+            hash[nodes.dup] = Nokogiri::XML::Builder.new do |xml|
+              add_nodes(xml, nodes.dup)
+            end.doc
+          end
+        end
+
+        def stub_match?(stack, node_xpath)
+          # Only true if the xpath matches the full stack:
+          stubs[stack].at_xpath(node_xpath) &&
+            !stubs[stack[0..-2]].at_xpath(node_xpath)
+        end
+
+        def stream_xml_nodes(file_data, node_xpath, &block)
+          require 'nokogiri'
+
+          # Track nesting as the cursor moves through the document:
+          stack       = []
+          match_depth = nil
+
+          Nokogiri::XML::Reader(file_data).each do |node|
+            case node.node_type
+            when 1 then # "start element"
+              stack.push [node.name, node.attributes]
+
+              if match_depth || !stub_match?(stack, node_xpath)
+                stack.pop if node.empty_element?
+                next
+              end
+
+              # "empty element" matches are yielded immediately, without
+              # tagging the stack as having matched, because there won't
+              # be an equivalent closing tag to end the match with later.
+              if node.empty_element?
+                stack.pop
+              else
+                match_depth = stack.length
+              end
+
+              block.call Nokogiri::XML(node.outer_xml).at("./#{node.name}")
+            when 15 then # "end element"
+              stack.pop
+              match_depth = nil if match_depth && stack.length < match_depth
+            end
+          end
+        end
+      end
+    end
+  end
+end
---
+++ test/helpers/file/xml_streaming_test.rb
@@ -0,0 +1,53 @@
+require 'test_helper'
+require 'ndr_import/helpers/file/xml_streaming'
+require 'nokogiri'
+
+# XML streaming file helper tests
+class XmlTest < ActiveSupport::TestCase
+  # This is a test importer class to test the XML streaming file helper mixin
+  class TestImporter
+    include NdrImport::Helpers::File::XmlStreaming
+
+    def nodes(xpath, xml)
+      [].tap do |nodes|
+        stream_xml_nodes(xml, xpath) { |node| nodes << node }
+      end
+    end
+  end
+
+  def setup
+    @importer = TestImporter.new
+  end
+
+  test 'should yield matching nodes' do
+    assert_equal 2, @importer.nodes('//node', <<~XML).length
+      <nodes><node></node><node></node></nodes>
+    XML
+  end
+
+  test 'should yield matching nodes with attributes' do
+    assert_equal 1, @importer.nodes('//nodes[@zone="a"]//node[@type="1"]', <<~XML).length
+      <root>
+        <nodes zone="a"><node type="1"></node><node type="2"></node></nodes>
+        <nodes zone="b"><node type="1"></node><node type="2"></node></nodes>
+      </root>
+    XML
+  end
+
+  test 'should yield matching empty_element nodes' do
+    assert_equal 2, @importer.nodes('//node', <<~XML).length
+      <nodes><node/><node/></nodes>
+    XML
+  end
+
+  test 'should yield nokogiri elements' do
+    node = @importer.nodes('//node', <<~XML).first
+      <nodes><node><foo>bar</foo></node></nodes>
+    XML
+
+    assert_kind_of Nokogiri::XML::Element, node
+    assert_equal 'node', node.name
+    assert_equal ['foo'], node.children.map(&:name)
+    assert_equal 'bar', node.text
+  end
+end
