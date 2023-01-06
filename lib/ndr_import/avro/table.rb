e2ab1d4cefe7a637d06b797f87dfe4b6bd7b517d
ollietulloch <oliver.tulloch@phe.gov.uk>
2023-01-06 18:18:15 +0000

    # Use NdrImport::Table to validate the header

--- lib/ndr_import/avro/table.rb
+++ lib/ndr_import/avro/table.rb
@@ -25,22 +25,12 @@ module NdrImport
       def transform_line(line, index)
         return enum_for(:transform_line, line, index) unless block_given?
 
-        # The first line will always be the "header"
-        validate_header(line) if index.zero?
-
         masked_mappings.each do |klass, klass_mappings|
           fields = mapped_line(line.values.map(&:to_s), klass_mappings)
           next if fields[:skip].to_s == 'true'.freeze
 
           yield(klass, fields, index)
         end
-      end
-
-      private
-
-      def validate_header(line)
-        consume_header_line(line, @columns)
-        fail_unless_header_complete(@columns) unless @header_valid
       end
     end
   end
