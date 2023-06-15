94eda331be377f09d48b467d273aecd2ff543a4e
bshand <bshand@users.noreply.github.com>
2023-06-15 18:57:58 +0100

    # Support Ruby 3.2. Drop support for Ruby 2.7, Rails 6.0 (#106)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,6 @@
 ## [Unreleased]
-*no unreleased changes*
+### Added
+* Support Ruby 3.2. Drop support for Ruby 2.7, Rails 6.0
 
 ## 10.2.0 / 2023-05-16
 * avro file support
--- test/helpers/file/word_test.rb
+++ test/helpers/file/word_test.rb
@@ -20,7 +20,12 @@ class WordTest < ActiveSupport::TestCase
 
   test 'read_word_file helper should raise exception on invalid word file' do
     assert_raises RuntimeError do
-      @importer.send(:read_word_file, @permanent_test_files.join('not_a_word_file.doc'))
+      _out, _err = capture_subprocess_io do
+        # capture_subprocess_io avoids noisy tests, otherwise ruby-ole prints to stderr:
+        # [ .../lib/ruby/gems/3.0.0/gems/ruby-ole-1.2.12.2/lib/ole/storage/base.rb:297:clear]
+        # WARN   creating new ole storage object on non-writable io
+        @importer.send(:read_word_file, @permanent_test_files.join('not_a_word_file.doc'))
+      end
     end
   end
 end
