8d35cb2747fe8b900fcda3e0cbb732c47e3f35df
ollietulloch <ollie.tulloch@nhs.net>
2023-09-13 09:12:42 +0100

    reverting for now

--- lib/ndr_import/xml/table.rb
+++ lib/ndr_import/xml/table.rb
@@ -31,13 +31,11 @@ module NdrImport
 
         xml_line = column_xpaths.map { |column_xpath| line.xpath(column_xpath).inner_text }
 
-        xml_records = masked_mappings.map do |klass, klass_mappings|
+        masked_mappings.each do |klass, klass_mappings|
           fields = mapped_line(xml_line, klass_mappings)
           next if fields[:skip].to_s == 'true'.freeze
-
-          [klass, fields, index]
+          yield(klass, fields, index)
         end
-        yield(xml_records.compact)
       end
 
       private
