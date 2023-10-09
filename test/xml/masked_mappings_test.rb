ff319f45fb94e485681e59a9949923c8c689e952
ollietulloch <ollie.tulloch@nhs.net>
2023-10-09 12:52:11 +0100

    improve masked mapping tests

--- test/xml/masked_mappings_test.rb
+++ test/xml/masked_mappings_test.rb
@@ -3,7 +3,6 @@ require 'test_helper'
 # This tests the NdrImport::Xml::MaskedMapping class
 module Xml
   class MaskedMappingsTest < ActiveSupport::TestCase
-
     def test_should_build_masked_mappings_with_column_specific_klasses
       masked_mapping = NdrImport::Xml::MaskedMappings.new(nil, xml_column_mapping).call
 
@@ -20,7 +19,7 @@ module Xml
 
     def xml_column_mapping
       [
-        { 'column' => 'no_relative_path', 'klass' => 'SomeTestKlass',
+        { 'column' => 'no_relative_path', 'klass' => 'SomeTestKlass#bob',
           'xml_cell' => { 'relative_path' => '', 'attribute' => 'value' } },
         { 'column' => 'no_relative_path', 'klass' => 'SomeTestKlass',
           'rawtext_name' => 'no_relative_path_inner_text',
@@ -31,14 +30,22 @@ module Xml
           'xml_cell' => { 'relative_path' => 'demographics' } },
         { 'column' => 'demographics_2', 'klass' => 'SomeTestKlass#2',
           'xml_cell' => { 'relative_path' => 'demographics', 'attribute' => 'code' } },
-        { 'column' => 'demographics_2', 'klass' => 'SomeTestKlass#',
+        { 'column' => 'demographics_2', 'klass' => 'SomeTestKlass#2',
           'rawtext_name' => 'demographics_2_inner_text',
           'xml_cell' => { 'relative_path' => 'demographics' } }
       ]
     end
 
     def expected_masked_mappings
-      { 'SomeTestKlass#1' =>
+      { 'SomeTestKlass#bob' =>
+        [{ 'column' => 'no_relative_path', 'klass' => 'SomeTestKlass#bob',
+           'xml_cell' => { 'relative_path' => '', 'attribute' => 'value' } },
+         { 'do_not_capture' => true },
+         { 'do_not_capture' => true },
+         { 'do_not_capture' => true },
+         { 'do_not_capture' => true },
+         { 'do_not_capture' => true }],
+        'SomeTestKlass#1' =>
         [{ 'do_not_capture' => true },
          { 'do_not_capture' => true },
          { 'column' => 'no_path_or_att', 'klass' => 'SomeTestKlass#1',
@@ -54,16 +61,9 @@ module Xml
          { 'do_not_capture' => true },
          { 'column' => 'demographics_2', 'klass' => 'SomeTestKlass#2',
            'xml_cell' => { 'relative_path' => 'demographics', 'attribute' => 'code' } },
-         { 'do_not_capture' => true }],
-        'SomeTestKlass#' =>
-        [{ 'do_not_capture' => true },
-         { 'do_not_capture' => true },
-         { 'do_not_capture' => true },
-         { 'do_not_capture' => true },
-         { 'do_not_capture' => true },
-         { 'column' => 'demographics_2', 'klass' => 'SomeTestKlass#',
-           'rawtext_name' => 'demographics_2_inner_text', 'xml_cell' =>
-             { 'relative_path' => 'demographics' } }] }
+         { 'column' => 'demographics_2', 'klass' => 'SomeTestKlass#2',
+           'rawtext_name' => 'demographics_2_inner_text',
+           'xml_cell' => { 'relative_path' => 'demographics' } }] }
     end
   end
 end
