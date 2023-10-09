fbdb4f50a06f2791a1c8e3f0c5782a47816ed13c
ollietulloch <ollie.tulloch@nhs.net>
2023-10-09 16:31:16 +0100

    Allow for an array of klasses

--- lib/ndr_import/xml/column_mapping.rb
+++ lib/ndr_import/xml/column_mapping.rb
@@ -19,24 +19,39 @@ module NdrImport
         new_column['column']                    = unmapped_node_parts[:column_name]
         new_column['xml_cell']['relative_path'] = unmapped_node_parts[:column_relative_path]
 
-        repeating_item   = existing_column.dig('xml_cell', 'multiple')
-        section_xpath    = existing_column.dig('xml_cell', 'section')
-        build_new_record = existing_column.dig('xml_cell', 'build_new_record')
+        repeating_item = existing_column.dig('xml_cell', 'multiple')
 
         # create unique rawtext names for repeating sections within a record
         new_column['rawtext_name'] = new_rawtext_name(new_column) if repeating_item
 
-        # If a table level klass is defined, there is nothing to increment at the column level.
-        # Similarly, not all repeating sections/items require a separate record.
-        # No need to create new records for a single occurence of a repeating section
-        no_new_record = klass.present? || build_new_record == false ||
-                        (repeating_item && xml_line.xpath(section_xpath).one?)
-        new_column['klass'] = existing_column['klass'] + "##{klass_increment}" unless no_new_record
+        return new_column if new_record_not_needed?(repeating_item)
 
+        new_column['klass'] = incremented_klass
         new_column
       end
 
       private
+
+      # If a table level klass is defined, there is nothing to increment at the column level.
+      # Similarly, not all repeating sections/items require a separate record.
+      # No need to create new records for a single occurence of a repeating section
+      def new_record_not_needed?(repeating_item)
+        section_xpath    = existing_column.dig('xml_cell', 'section')
+        build_new_record = existing_column.dig('xml_cell', 'build_new_record')
+
+        klass.present? || build_new_record == false ||
+          (repeating_item && xml_line.xpath(section_xpath).one?)
+      end
+
+      def incremented_klass
+        if existing_column['klass'].is_a?(Array)
+          existing_column['klass'].map do |column_klass|
+            column_klass + "##{klass_increment}"
+          end
+        else
+          existing_column['klass'] + "##{klass_increment}"
+        end
+      end
 
       # Append "_1", "_2" etc to repeating rawtext names within a single record
       def new_rawtext_name(new_column)
